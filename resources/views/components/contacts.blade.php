d93ebb4c955b8e8ab1a3370a8dba84c0c57c6fd1
niceddev <niceddeveloper@gmail.com>
2022-10-17 21:43:42 +0600

    add: add map

--- resources/views/components/contacts.blade.php
+++ resources/views/components/contacts.blade.php
@@ -2,31 +2,34 @@
     <div class="footer__container _container">
         <div class="footer__body">
             <h5 class="footer__title" id="{{ $sections['contacts']->slug }}">{{ $sections['contacts']->getTranslation('title', session()->get('lang', 'ru')) }}</h5>
-            <div class="footer__info">
-                <div class="footer__block contacts">
-                    <h6 class="contacts__title">{{ __('common.our_contacts') }}</h6>
-                    <div class="contacts__info">
-                        <div class="contacts__adress">
-                            <p>{{ $contacts->getTranslation('address', session()->get('lang', 'ru')) }}</p>
-                            <a href="">Показать на карте</a>
-                        </div>
-                        <div class="contacts__phone"><a href="tel:{{ $contacts->phone_number }}">{{ $contacts->phone_number }}</a></div>
-                        <div class="contacts__phone"><a href="">{{ $contacts->whatsapp_number }}</a></div>
-                        <div class="contacts__mail"><a href="{{ $contacts->email }}">{{ $contacts->email }}</a></div>
-                    </div>
-                </div>
-                <div class="footer__block contacts">
-                    <h6 class="contacts__title">{{ __('common.opening_hours') }}</h6>
-                    <div class="contacts__info">
-                        <div class="contacts__time">
-                            {!! $contacts->getTranslation('graphic', session()->get('lang', 'ru'))  !!}
+            <div class="contacts__flex">
+                <img class="contacts__img" src="/img/map.jpg" alt="">
+                <div class="footer__info">
+                    <div class="footer__block contacts">
+                        <h6 class="contacts__title">{{ __('common.our_contacts') }}</h6>
+                        <div class="contacts__info">
+                            <div class="contacts__adress">
+                                <p>{{ $contacts->getTranslation('address', session()->get('lang', 'ru')) }}</p>
+                                <a href="">Показать на карте</a>
+                            </div>
+                            <div class="contacts__phone"><a href="tel:{{ $contacts->phone_number }}">{{ $contacts->phone_number }}</a></div>
+                            <div class="contacts__phone"><a href="">{{ $contacts->whatsapp_number }}</a></div>
+                            <div class="contacts__mail"><a href="{{ $contacts->email }}">{{ $contacts->email }}</a></div>
                         </div>
                     </div>
-                    <h6 class="contacts__title">{{ __('common.social_network') }}</h6>
-                    <div class="contacts__info">
-                        <div class="contacts__social">
-                            <a href="" class="contacts__icon"><img src="img/icons/insta.svg" alt=""></a>
-                            <a href="" class="contacts__icon"><img src="img/icons/whp.svg" alt=""></a>
+                    <div class="footer__block contacts">
+                        <h6 class="contacts__title">{{ __('common.opening_hours') }}</h6>
+                        <div class="contacts__info">
+                            <div class="contacts__time">
+                                {!! $contacts->getTranslation('graphic', session()->get('lang', 'ru'))  !!}
+                            </div>
+                        </div>
+                        <h6 class="contacts__title">{{ __('common.social_network') }}</h6>
+                        <div class="contacts__info">
+                            <div class="contacts__social">
+                                <a href="" class="contacts__icon"><img src="img/icons/insta.svg" alt=""></a>
+                                <a href="" class="contacts__icon"><img src="img/icons/whp.svg" alt=""></a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/components/gallery.blade.php
+++ resources/views/components/gallery.blade.php
@@ -3,10 +3,10 @@
         <h5 class="gallery__title" id="{{ $sections['gallery']->slug }}">{{ $sections['gallery']->getTranslation('title', session()->get('lang', 'ru')) }}</h5>
         <div class="gallery__body">
             <ul class="gallery__list">
-                @foreach($gallery as $images)
-                    <li class="gallery__items">
+                @foreach($gallery as $image)
+                    <li data="img-{{ $image->id }}" class="gallery__items popup-link">
                         <a href="" class="gallery__item">
-                            <img src="{{ $images->path }}" alt="">
+                            <img src="{{ $image->path }}" alt="">
                         </a>
                     </li>
                 @endforeach
@@ -14,3 +14,11 @@
         </div>
     </div>
 </section>
+
+@foreach($gallery as $image)
+    <div class="popup" id="img-{{ $image->id }}">
+        <div class="popup__container">
+            <img src="{{ $image->path }}" alt="">
+        </div>
+    </div>
+@endforeach
